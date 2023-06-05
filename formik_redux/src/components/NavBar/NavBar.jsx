335f1793fac044616c9e42a07908878e10e58873
Aparajit-Garg <aparajit.garg06@gmail.com>
2023-06-06 01:11:20 +0530

    adding drop down for different options and tags to be displayed on show page

--- formik_redux/src/components/NavBar/NavBar.jsx
+++ formik_redux/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import CreateForm from "../CreateForm/CreateForm";
-import ShowData from "../ShowData/ShowData";
 import classes from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 import { searchText, searchGender, showUsers } from "../../features/userDetailSlice";
+import CreateForm from "../CreateForm/CreateForm";
+import ShowData from "../ShowData/ShowData";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
 
 const NavBar = () => {
 
+    const [selectedItem, setSelectedItem] = useState();
     const [searchTextData, setSearchTextData] = useState("");
     const [searchGenderValue, setSearchGenderValue] = useState("");
     const dispatch = useDispatch();
@@ -23,25 +26,30 @@ const NavBar = () => {
         dispatch(showUsers());
     }, [])
 
+
     return (
         <nav className={classes.nav__bar}>
             <div>
                 <Link to="/" element={<NavBar />}>Home</Link>
-                <Link to="/add" element={<CreateForm />}> Add new employee </Link>
-                <Link to="/showDirectory" element={<ShowData />}> Show employee directory ({allPosts.length}) </Link>
                 <div>
-                    <span>
-                        <input type="radio" name="gender" value="All" defaultChecked onClick={(e)=> setSearchGenderValue(e.target.value)}/>
-                        <label> All </label>
-                    </span>
-                    <span>
-                        <input type="radio" name="gender" value="Male" onClick={(e)=> setSearchGenderValue(e.target.value)} />
-                        <label> Male </label>
-                    </span>
-                    <span>
-                        <input type="radio" name="gender" value="Female" onClick={(e)=> setSearchGenderValue(e.target.value)}/>
-                        <label> Female </label>
-                    </span>
+                    <select value={selectedItem} onChange={(e) => setSelectedItem(e.target.value)}>
+                        <option>Directory Options</option>
+                        <option>
+                            <Link to="/add" element={<CreateForm />}> Add new employee </Link>
+                        </option>
+                        <option>
+                            <Link to="/showDirectory" element={<ShowData />}> Show employee directory ({allPosts.length}) </Link>
+                        </option>
+                    </select>
+                    <ArrowDropDownIcon className={classes.dropDown} />
+                </div>
+                
+                <div style={{display: "none"}} className={classes.tags}>
+                    <span>Hardware Designing</span>
+                    <span>IT</span>
+                    <span>Security Analyst</span>
+                    <span>Actor</span>
+                    {/* <span>Software Architect</span> */}
                 </div>
             </div>
             <div>
